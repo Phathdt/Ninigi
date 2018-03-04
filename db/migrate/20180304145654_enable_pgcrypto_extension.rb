7fc118a2885d935b76e9b5323f9d4e84ee7d5dee
Phathdt <phathdt.leviateam@gmail.com>
2018-03-04 22:43:18 +0700

    use uuid for session code

--- db/migrate/20180304145654_enable_pgcrypto_extension.rb
+++ db/migrate/20180304145654_enable_pgcrypto_extension.rb
@@ -1,5 +1,6 @@
 class EnablePgcryptoExtension < ActiveRecord::Migration[5.1]
   def change
     enable_extension 'pgcrypto'
+    enable_extension 'uuid-ossp'
   end
 end
--- db/migrate/20180304150650_create_sessions.rb
+++ db/migrate/20180304150650_create_sessions.rb
@@ -2,11 +2,11 @@ class CreateSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :sessions, id: :uuid do |t|
       t.references :user, foreign_key: true, type: :uuid
-      t.string :sessiion_code, limit: 36
+      t.uuid :token, null: false, default: 'uuid_generate_v4()'
       t.boolean :active, default: true
 
       t.timestamps
     end
-    add_index :sessions, :sessiion_code, unique: true
+    add_index :sessions, :token, unique: true
   end
 end
