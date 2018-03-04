5ba49b3473ea27f659bac89d952e5b4d76836d40
Phathdt <phathdt.leviateam@gmail.com>
2018-03-04 22:08:57 +0700

    add model session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :sessions, dependent: :destroy
 end
---
+++ db/migrate/20180304150650_create_sessions.rb
@@ -0,0 +1,12 @@
+class CreateSessions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :sessions, id: :uuid do |t|
+      t.references :user, foreign_key: true
+      t.string :sessiion_code, limit: 36
+      t.boolean :active, default: true
+
+      t.timestamps
+    end
+    add_index :sessions, :sessiion_code, unique: true
+  end
+end
