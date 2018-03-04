4c3e6b44e67f09cda26bb8b3cc3cc92fcb8144fc
Phathdt <phathdt.leviateam@gmail.com>
2018-03-04 22:22:04 +0700

    change references to uuid

--- db/migrate/20180304150650_create_sessions.rb
+++ db/migrate/20180304150650_create_sessions.rb
@@ -1,7 +1,7 @@
 class CreateSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :sessions, id: :uuid do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, type: :uuid
       t.string :sessiion_code, limit: 36
       t.boolean :active, default: true
 
