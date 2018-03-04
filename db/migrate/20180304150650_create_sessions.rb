285fd32a31de061001c69418b74cbdca493ed02e
Phathdt <phathdt.leviateam@gmail.com>
2018-03-05 02:26:31 +0700

    recode migration

--- db/migrate/20180304150650_create_sessions.rb
+++ db/migrate/20180304150650_create_sessions.rb
@@ -1,12 +1,13 @@
 class CreateSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :sessions, id: :uuid do |t|
-      t.references :user, foreign_key: true, type: :uuid
-      t.uuid :token, null: false, default: 'uuid_generate_v4()'
-      t.boolean :active, default: true
+      t.references :user,   foreign_key: true,  type:    :uuid
+      t.uuid       :token,  null:        false, default: 'uuid_generate_v4()'
+      t.boolean    :active, default:     true
 
       t.timestamps
     end
+
     add_index :sessions, :token, unique: true
   end
 end
