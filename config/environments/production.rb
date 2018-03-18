aa944b83d105c8fe6d2918d74f719f0c9cb20628
Phathdt <phathdt.leviateam@gmail.com>
2018-03-18 12:28:35 +0700

    remove port for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'], port: ENV['PORT'] }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'] }
 
   config.action_mailer.delivery_method = :smtp
 
