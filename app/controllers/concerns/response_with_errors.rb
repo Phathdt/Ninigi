ca3da742d971264cc6c1f614ca29912233d1d2d1
Phathdt <phathdt.leviateam@gmail.com>
2018-03-18 10:49:08 +0700

    add catch error record not found

--- app/controllers/concerns/response_with_errors.rb
+++ app/controllers/concerns/response_with_errors.rb
@@ -4,20 +4,22 @@ module ResponseWithErrors
   included do
     rescue_from Error::NotAuthenticated, with: :not_authenticated
     rescue_from Error::TokenNotMatch, with: :token_not_match
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   end
 
   private
 
   def not_authenticated
-    render_error(I18n.t('.not_authenticated'), :unauthorized)
+    render_error(I18n.t('devise.failure.invalid'), :unauthorized)
   end
 
   def token_not_match
-    render_error(I18n.t('.token_not_match'), :unauthorized)
+    render_error(I18n.t('devise.failure.invalid_token'), :unauthorized)
   end
 
-  def record_not_found
-    render_error(I18n.t('.record_not_found'), :not_found)
+  def record_not_found(exception)
+    class_object = exception.message.match(/Couldn't find ([\w]+)/)[1]
+    render_error(I18n.t('activerecord.errors.messages.record_not_found', resourse: class_object), :not_found)
   end
 
   def render_error(message, status)
