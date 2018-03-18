797f8d6588c1478d0d235553ed11d052c3490f9b
Phathdt <phathdt.leviateam@gmail.com>
2018-03-18 12:27:42 +0700

    update mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'], port: ENV['PORT'] }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    port: ENV['SMTP_PORT'],
+    address: ENV['SMTP_SERVER'],
+    user_name: ENV['SMTP_LOGIN'],
+    password: ENV['SMTP_PASSWORD'],
+    domain: ENV['DOMAIN_NAME'],
+    authentication: :plain
+  }
 end
