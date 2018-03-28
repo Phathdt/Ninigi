ca87be51b11e10a2255d2f6f616326f804c36b75
Phathdt <phathdt.leviateam@gmail.com>
2018-03-28 21:21:54 +0700

    change config mailer delayjob

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,12 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'], port: ENV['PORT'] }
 
+  config.action_mailer.default_options = { from: ENV['SMTP_LOGIN'] }
+
+  config.action_mailer.default charset: 'utf-8'
+
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
