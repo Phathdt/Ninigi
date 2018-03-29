f8af766c216283b736c03c66955b40400bbffdeb
Phathdt <phathdt.leviateam@gmail.com>
2018-03-29 23:17:09 +0700

    add update, destroy restaurant, change show restaurant

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,7 @@
 module Api::V1
   class RestaurantsController < BaseApiController
-    prepend_before_action :authenticate_request!, only: %i[create]
+    prepend_before_action :authenticate_request!, only: %i[create update destroy]
+    before_action :find_restaurant, only: %i[show update destroy]
 
     def index
       data, status = service.index
@@ -8,8 +9,7 @@ module Api::V1
     end
 
     def show
-      data, status = service.show(params[:id])
-      render_json(data, status)
+      render_json(@restaurant, :ok)
     end
 
     def create
@@ -17,7 +17,23 @@ module Api::V1
       render_json(data, status)
     end
 
+    def update
+      authorize @restaurant
+      data, status = service.update(@restaurant, restaurant_params)
+      render_json(data, status)
+    end
+
+    def destroy
+      authorize @restaurant
+      data, status = service.destroy(@restaurant)
+      render_json(data, status)
+    end
+
     private
+
+    def find_restaurant
+      @restaurant ||= Restaurant.find(params[:id])
+    end
 
     def restaurant_params
       params.require(:restaurants).permit(:name, :address, :phone, :desc)
--- app/services/restaurant_service.rb
+++ app/services/restaurant_service.rb
@@ -4,14 +4,18 @@ class RestaurantService < BaseService
     [restaurants, :ok]
   end
 
-  def show(id)
-    restaurant = Restaurant.find(id)
-    [restaurant, :ok]
-  end
-
   def create(params)
     restaurant = current_user.restaurants.new(params)
     message = simple_create(restaurant, options)
     render_message(message)
+  end
+
+  def update(restaurant, params)
+    message = simple_update(restaurant, params, options)
+    render_message(message)
+  end
+
+  def destroy(restaurant)
+    message = custom_destroy restaurant
   end
 end
