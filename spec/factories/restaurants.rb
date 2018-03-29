6245d77a9298b234ed2176f6cb9ef834a15fedd6
Phathdt <phathdt.leviateam@gmail.com>
2018-03-30 01:46:30 +0700

    add spec

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :restaurant do
+    name 'MyString'
+    address 'MyString'
+    phone 'MyString'
+    desc 'MyText'
+    latitude 1.5
+    longitude 1.5
+    user nil
+  end
+end
