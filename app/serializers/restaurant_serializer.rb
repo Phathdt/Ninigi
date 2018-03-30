813000bd09ad67ccd3d90cf427ae66be6d20f595
Phathdt <phathdt.leviateam@gmail.com>
2018-03-30 21:56:53 +0700

    add misssing space

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -4,6 +4,6 @@ class RestaurantSerializer < BaseSerializer
   set_key_transform :camel_lower
 
   attribute :isAdmin do |object|
-    current_user.has_role?(:admin) ? "tao la admin tao co quyen" : {"em la thuong dan": 1}
+    current_user.has_role?(:admin) ? 'tao la admin tao co quyen' : { "em la thuong dan": 1 }
   end
 end
