2691792c02d4fc95d05d07782f50e533a939ad7a
Phathdt <phathdt.leviateam@gmail.com>
2018-03-30 22:27:22 +0700

    add service admin to admin controllers

--- app/controllers/api/v1/admin/admin_controller.rb
+++ app/controllers/api/v1/admin/admin_controller.rb
@@ -1,6 +1,7 @@
 module Api::V1
   class Admin::AdminController < BaseApiController
     before_action :authorize_admin!
+    before_action :service_admin
 
     private
 
@@ -8,6 +9,11 @@ module Api::V1
       authenticate_request!
 
       raise Pundit::NotAuthorizedError unless current_user.has_role?(:admin)
+    end
+
+    def service_admin
+      service ||= "Admin::#{controller_name.singularize.capitalize}Service".constantize
+      @service ||= service.new(current_user, controller_name, action_name)
     end
   end
 end
