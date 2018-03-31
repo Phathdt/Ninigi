e324a319ef31cc5b673fa42b76d9f0d3db420fd2
Phathdt <phathdt.leviateam@gmail.com>
2018-03-31 11:55:30 +0700

    add service index admin restaurant

--- app/controllers/api/v1/admin/restaurants_controller.rb
+++ app/controllers/api/v1/admin/restaurants_controller.rb
@@ -1,6 +1,8 @@
 module Api::V1
   class Admin::RestaurantsController < Admin::AdminController
     def index
+      data, status = service_admin.index(params[:page])
+      render_json(data, status)
     end
   end
 end
--- app/services/admin/restaurant_service.rb
+++ app/services/admin/restaurant_service.rb
@@ -1,4 +1,8 @@
 module Admin
   class RestaurantService < BaseService
+    def index(page_number)
+      restaurants = Restaurant.page(page_number || 1)
+      [restaurants, :ok]
+    end
   end
 end
