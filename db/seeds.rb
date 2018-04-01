f926ff1bebd89279bc8d44a7af2efe0e3e86b233
Phathdt <phathdt.leviateam@gmail.com>
2018-04-02 03:24:22 +0700

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,17 +17,17 @@ admin.restaurants.create(
   phone: '096 962 68 00',
   album_images_attributes: [
     {
-      caption: "Donec eleifend diam id turpis placerat, non dignissim nibh pretium",
+      caption: 'Donec eleifend diam id turpis placerat, non dignissim nibh pretium',
       is_cover: true,
-      temp_url: "https://www.wien.info/media/images/50416-zu-den-3-hacken-restaurant-wiener-kueche-wiener-schnitzel.jpg/image_leading_article_teaser"
+      temp_url: 'https://www.wien.info/media/images/50416-zu-den-3-hacken-restaurant-wiener-kueche-wiener-schnitzel.jpg/image_leading_article_teaser'
     },
     {
-      caption: "Aenean non dapibus leo. Donec odio justo, volutpat ut finibus a, tempor id nibh. Vivamus at elit non ligula lobortis sollicitudin eleifend ac risus",
-      temp_url: "http://www.realdetroitweekly.com/wp-content/uploads/2017/06/Restaurants.jpg"
+      caption: 'Aenean non dapibus leo. Donec odio justo, volutpat ut finibus a, tempor id nibh. Vivamus at elit non ligula lobortis sollicitudin eleifend ac risus',
+      temp_url: 'http://www.realdetroitweekly.com/wp-content/uploads/2017/06/Restaurants.jpg'
     },
     {
-      caption: "Ut ac orci dignissim, fermentum lacus quis, gravida lorem. Phasellus eu nisl et quam eleifend ultrices. Proin hendrerit, nulla ut elementum pretium",
-      temp_url: "https://www.ashmolean.org/sites/default/files/styles/listing_landscape_textoverlay_left_bottom_image/public/ashmolean/images/media/cafe1.jpg?itok=RRq3Tds5"
+      caption: 'Ut ac orci dignissim, fermentum lacus quis, gravida lorem. Phasellus eu nisl et quam eleifend ultrices. Proin hendrerit, nulla ut elementum pretium',
+      temp_url: 'https://www.ashmolean.org/sites/default/files/styles/listing_landscape_textoverlay_left_bottom_image/public/ashmolean/images/media/cafe1.jpg?itok=RRq3Tds5'
     }
   ]
 )
@@ -39,17 +39,17 @@ admin.restaurants.create(
   phone: '028 7303 9079',
   album_images_attributes: [
     {
-      caption: "Donec eleifend diam id turpis placerat, non dignissim nibh pretium",
+      caption: 'Donec eleifend diam id turpis placerat, non dignissim nibh pretium',
       is_cover: true,
-      temp_url: "https://libertyhouserestaurant.com/wp-content/uploads/sites/13/2016/11/slide-restaurant.jpg"
+      temp_url: 'https://libertyhouserestaurant.com/wp-content/uploads/sites/13/2016/11/slide-restaurant.jpg'
     },
     {
-      caption: "Aenean non dapibus leo. Donec odio justo, volutpat ut finibus a, tempor id nibh. Vivamus at elit non ligula lobortis sollicitudin eleifend ac risus",
-      temp_url: "http://d1dzqwexhp5ztx.cloudfront.net/imageRepo/4/0/80/27/139/IMG_0020_1_2_S.jpg"
+      caption: 'Aenean non dapibus leo. Donec odio justo, volutpat ut finibus a, tempor id nibh. Vivamus at elit non ligula lobortis sollicitudin eleifend ac risus',
+      temp_url: 'http://d1dzqwexhp5ztx.cloudfront.net/imageRepo/4/0/80/27/139/IMG_0020_1_2_S.jpg'
     },
     {
-      caption: "Ut ac orci dignissim, fermentum lacus quis, gravida lorem. Phasellus eu nisl et quam eleifend ultrices. Proin hendrerit, nulla ut elementum pretium",
-      temp_url: "https://www.breezedevelopment.co.uk/wp-content/uploads/2017/07/CroppedImage1600900-restaurant-alpenhof-zermatt.jpg"
+      caption: 'Ut ac orci dignissim, fermentum lacus quis, gravida lorem. Phasellus eu nisl et quam eleifend ultrices. Proin hendrerit, nulla ut elementum pretium',
+      temp_url: 'https://www.breezedevelopment.co.uk/wp-content/uploads/2017/07/CroppedImage1600900-restaurant-alpenhof-zermatt.jpg'
     }
   ]
 )
@@ -61,17 +61,17 @@ admin.restaurants.create(
   phone: '028 3812 2569',
   album_images_attributes: [
     {
-      caption: "Donec eleifend diam id turpis placerat, non dignissim nibh pretium",
+      caption: 'Donec eleifend diam id turpis placerat, non dignissim nibh pretium',
       is_cover: true,
-      temp_url: "https://www.villalacoste.com/wp-content/uploads/2017/03/04b.jpg"
+      temp_url: 'https://www.villalacoste.com/wp-content/uploads/2017/03/04b.jpg'
     },
     {
-      caption: "Aenean non dapibus leo. Donec odio justo, volutpat ut finibus a, tempor id nibh. Vivamus at elit non ligula lobortis sollicitudin eleifend ac risus",
-      temp_url: "https://1nsw6u.akamaized.net/application/files/1914/7340/5572/jabalakhdar-juniper-restaurant.jpg"
+      caption: 'Aenean non dapibus leo. Donec odio justo, volutpat ut finibus a, tempor id nibh. Vivamus at elit non ligula lobortis sollicitudin eleifend ac risus',
+      temp_url: 'https://1nsw6u.akamaized.net/application/files/1914/7340/5572/jabalakhdar-juniper-restaurant.jpg'
     },
     {
-      caption: "Ut ac orci dignissim, fermentum lacus quis, gravida lorem. Phasellus eu nisl et quam eleifend ultrices. Proin hendrerit, nulla ut elementum pretium",
-      temp_url: "http://parkresto.com/wp-content/themes/parkrestaurant/images/11onlinereservationpark.jpg"
+      caption: 'Ut ac orci dignissim, fermentum lacus quis, gravida lorem. Phasellus eu nisl et quam eleifend ultrices. Proin hendrerit, nulla ut elementum pretium',
+      temp_url: 'http://parkresto.com/wp-content/themes/parkrestaurant/images/11onlinereservationpark.jpg'
     }
   ]
 )
