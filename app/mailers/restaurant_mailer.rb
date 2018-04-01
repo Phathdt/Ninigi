aa99c42bc4ac1511ff9879e901a9c61ecc9edf80
Phathdt <phathdt.leviateam@gmail.com>
2018-04-01 09:35:01 +0700

    change url restaurant at mailer

--- app/mailers/restaurant_mailer.rb
+++ app/mailers/restaurant_mailer.rb
@@ -8,8 +8,8 @@ class RestaurantMailer < ApplicationMailer
       email_content_from(:content,
                          state: state, name: receiver.name,
                          restaurant: restaurant.name, time: restaurant.updated_at.to_s,
-                         link: api_v1_restaurant_url(restaurant), comment: restaurant.comment,
-                         contact_email: ENV['REPLY_EMAIL'])
+                         link: api_v1_restaurant_url(restaurant).gsub('api/v1/', ''),
+                         comment: restaurant.comment, contact_email: ENV['REPLY_EMAIL'])
 
     mail(to: receiver.email, subject: subject)
   end
