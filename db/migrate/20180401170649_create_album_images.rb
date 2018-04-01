76b653fcc5dc3f584b80412992e5d65d780eeba4
Phathdt <phathdt.leviateam@gmail.com>
2018-04-02 03:45:15 +0700

    remove blank line

--- db/migrate/20180401170649_create_album_images.rb
+++ db/migrate/20180401170649_create_album_images.rb
@@ -2,10 +2,10 @@ class CreateAlbumImages < ActiveRecord::Migration[5.1]
   def change
     create_table :album_images, id: :uuid do |t|
       t.text       :caption
-      t.boolean    :is_cover,   default: false
+      t.boolean    :is_cover, default: false
       t.string     :temp_url
       t.attachment :photo
-      t.references :restaurant, type:    :uuid, foreign_key: true
+      t.references :restaurant, type: :uuid, foreign_key: true
 
       t.timestamps
     end
