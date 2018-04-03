d171babed1ab010ca8e9dc1a120b8696d616f510
Phathdt <phathdt.leviateam@gmail.com>
2018-04-03 23:15:51 +0700

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402172425) do
+ActiveRecord::Schema.define(version: 20180403154051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180402172425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.text "comment"
     t.index ["deleted_at"], name: "index_manager_requests_on_deleted_at"
     t.index ["restaurant_id", "user_id"], name: "index_manager_requests_on_restaurant_id_and_user_id", unique: true
     t.index ["restaurant_id"], name: "index_manager_requests_on_restaurant_id"
