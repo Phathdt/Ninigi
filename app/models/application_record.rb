c3e4eed55acf158e549c028011a9381797128eb1
Phathdt <phathdt.leviateam@gmail.com>
2018-04-10 00:09:40 +0700

    add sample scope

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   default_scope { order('created_at DESC') }
+
+  scope :unscope_order, -> { unscope(:order) }
+  scope :sample, -> { unscope_order.order("RANDOM()").first }
 end
