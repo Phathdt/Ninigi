29f1fa858d115f54c2ede4d465164ecd81fb4bb0
Phathdt <phathdt.leviateam@gmail.com>
2018-04-30 12:37:25 +0700

    add tables to order application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  default_scope { order('created_at DESC') }
+  default_scope { order("#{self.sti_name.pluralize.underscore}.created_at DESC") }
 
   scope :unscope_order, -> { unscope(:order) }
   scope :sample, -> { unscope_order.order('RANDOM()').first }
