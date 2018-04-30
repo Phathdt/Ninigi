86e53d1addc8eb744b809a51df4fe02a1d28c6fe
Phathdt <phathdt.leviateam@gmail.com>
2018-04-30 11:46:55 +0700

    update album image policy

--- app/policies/album_image_policy.rb
+++ app/policies/album_image_policy.rb
@@ -1,5 +1,6 @@
 class AlbumImagePolicy < ApplicationPolicy
   def destroy?
-    record.restaurant.owner == user
+    record.restaurant.owner == user ||
+      record.restaurant.managers.where(id: user).exists?
   end
 end
