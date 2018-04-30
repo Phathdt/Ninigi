455a7717690ae77801bf21f8e85a5b503a2a8720
Phathdt <phathdt.leviateam@gmail.com>
2018-04-30 12:41:26 +0700

    add review to v2

---
+++ app/controllers/api/v2/owner/reviews_controller.rb
@@ -0,0 +1,40 @@
+module Api::V2
+  class ReviewsController < BaseApiController
+    before_action :find_reviewable, only: %i[index]
+    before_action :find_review, only: %i[destroy]
+
+    def index
+      data, status = service.index(@reviewable, params[:page])
+      render_json(data, status)
+    end
+
+    def destroy
+      authorize @review
+      data, status = service.destroy(@review)
+      render_json(data, status)
+    end
+
+    private
+
+    def find_reviewable
+      @reviewable ||=
+        if params[:restaurant_id].present?
+          Restaurant.find(params[:restaurant_id])
+        else
+          Dish.find(params[:dish_id])
+        end
+    end
+
+    def find_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:reviews).permit(:content, :point, images_attr)
+    end
+
+    def images_attr
+      { images_attributes: %i[id caption is_cover temp_url _destroy] }
+    end
+  end
+end
