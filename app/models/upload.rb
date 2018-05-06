dccf20b16a44706e1472ea63251d41edb1fb28c1
Phathdt <phathdt.leviateam@gmail.com>
2018-05-06 21:47:03 +0700

    set delayjob delete upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,8 @@
 class Upload < ApplicationRecord
   has_attached_file :file
 
+  after_create { delay(run_at: 7.days.from_now).destroy }
+
   validates_attachment :file, size: { in: 0..MAX_FIZESIZE },
     content_type: { content_type: CONTENT_TYPE_PATTERN }
 end
